d30d941bfea69b90c8cb720a69f130379624d8c7
tj <theodore.hoff@gmail.com>
2024-12-11 20:15:30 -0700

    WIP

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,18 +1,35 @@
-use std::{collections::LinkedList, fs::read_to_string};
+use std::{collections::LinkedList, fs::read_to_string, str::Chars};
 
-use itertools::Itertools;
+use itertools::{Chunks, Itertools};
 
 #[derive(Clone)]
-enum Block {
+enum BlockType {
     FILE { id: usize },
     EMPTY,
+}
+
+#[derive(Clone)]
+struct Block {
+    size: u32,
+    block: BlockType,
+}
+
+fn find_containing_block(
+    blocks: &Vec<Block>,
+    current: &Block,
+    index: usize,
+) -> Option<usize> {
+    return Some(
+        blocks[0..index]
+            .into_iter()
+            .find_position(|b| b.size < current.size)?
+            .0,
+    );
 }
 
 fn main() {
     let filename = "data.txt";
     let file_text = read_to_string(filename).unwrap();
-
-    let parse_digit = |c: char| c.to_digit(10).unwrap();
 
     let fill_with = |num: u32, item: Block| -> Vec<_> {
         { 1..=num }.into_iter().map(|_x| item.clone()).collect()
@@ -23,55 +40,48 @@ fn main() {
         .chunks(2)
         .into_iter()
         .enumerate()
-        .map(|(id, (mut chunk))| {
+        .map(|(id, chunk)| {
+            let parse_digit = |c: char| c.to_digit(10).unwrap();
+
             let fill_amount = parse_digit(chunk.next().unwrap());
 
-            let mut full = fill_with(fill_amount, Block::FILE { id: id });
+            let file_block = Block {
+                size: fill_amount,
+                block: BlockType::FILE { id: id },
+            };
+
+            let blocks = vec![file_block];
             if let Some(possible_empty) = chunk.next() {
                 let empty_amount = parse_digit(possible_empty);
-                let mut empties = fill_with(empty_amount, Block::EMPTY);
-                full.append(&mut empties);
+                let empty_block = Block {
+                    size: empty_amount,
+                    block: BlockType::EMPTY,
+                };
+                blocks.push(empty_block);
             }
-            return full;
+            return blocks;
         })
         .flatten()
         .collect();
 
-    let mut llblocks: LinkedList<Block> = blocks.into_iter().collect();
+    let reordered_blocks = blocks.iter().rev();
 
-    let mut defragged: Vec<usize> = Vec::new();
-
-    loop {
-        let Some(block) = llblocks.pop_front() else {
-            break;
+    let size = blocks.len();
+    for (index_from_back, block) in reordered_blocks.enumerate() {
+        let Some(swap_block) = find_containing_block(&blocks, block, index) else {
+            continue;
         };
 
-        // println!("{defragged:?}");
+        let block_with_room = blocks.get(swap_block).unwrap();
 
-        match block {
-            Block::FILE { id } => defragged.push(id),
-            Block::EMPTY => {
-                // find the first not empty block and push that instead.
-                loop {
-                    let Some(end_block) = llblocks.pop_back() else {
-                        break;
-                    };
-
-                    match end_block {
-                        Block::FILE { id } => {
-                            defragged.push(id);
-                            break;
-                        }
-                        Block::EMPTY => continue,
-                    }
-                }
-            }
-        }
+        let remaining_room = block_with_room.size - block.size;
+        block_with_room.size = remaining_room;
+        blocks.insert(swap_block, block.clone());
     }
 
     // println!("{defragged:?}");
 
-    let checksum: usize = defragged
+    let checksum: usize = blocks
         .into_iter()
         .enumerate()
         .map(|(idx, id)| idx * id)
